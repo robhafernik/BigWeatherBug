2fd9ccdc276ba7c293b16d3f409cf8f7d9b2b03b
robhafernik <rob@hafernik.com>
2021-03-17 16:34:24 -0500

    Added more descriptions, tidied things

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # BigWeatherBug
+
+
+From the big_bug.py file in this project:
+
+#
+# This project implements a "Big"WeatherBug", a small, enclosed display
+# that can be mounted on the wall to show the current temperature,
+# weather conditions, and forecast.  It is written in Python using
+# the Python game framework.
+# 
+# This uses the Raspberry Pi 3 single board computer, using the 
+# 'PiTFT Plus 480x320 3.5" TFT+Touchscreen for Raspberry Pi' from 
+# Adafruit for a display.  See https://www.adafruit.com/product/2441
+# 
+# The case is also from Adafruit and is specifically designed to fit this
+# particular screen.  See https://www.adafruit.com/product/2779
+# 
+# This code uses weather data from OpenWeatherMap: http://openweathermap.org
+#
+# Adafruit is an excellent source for hobby electronics, check them out today!
+#
+# Written by Rob Hafernik, rob@hafernik.com
+#
+# My first go at this was the "Little Weather Bug", which is documented
+# on GitHub at: https://github.com/robhafernik/LittleWeatherBug
+# 
+# This new version uses the display referenced above, along with the
+# pygame library for python to draw the display.  Some complicaton is 
+# added by having two color levels for everything: one for daytime and 
+# one for nighttime (the day mode is just too bright at night).
+#
+# Released into the public domain, do with it as you will.  It is not,
+# however, warranted to be correct in any way.
+#
--- big_bug.py
+++ big_bug.py
@@ -6,11 +6,37 @@ import os
 from settings import Settings
 from weather import Weather
 
-# This is the main class for the weather bug project
 #
-# This class should be instantiated and its run_bug method called to kick things off.
-# Everything after that should run naturally.
+# This project implements a "Big"WeatherBug", a small, enclosed display
+# that can be mounted on the wall to show the current temperature,
+# weather conditions, and forecast.  It is written in Python using
+# the Python game framework.
+# 
+# This uses the Raspberry Pi 3 single board computer, using the 
+# 'PiTFT Plus 480x320 3.5" TFT+Touchscreen for Raspberry Pi' from 
+# Adafruit for a display.  See https://www.adafruit.com/product/2441
+# 
+# The case is also from Adafruit and is specifically designed to fit this
+# particular screen.  See https://www.adafruit.com/product/2779
+# 
+# This code uses weather data from OpenWeatherMap: http://openweathermap.org
 #
+# Adafruit is an excellent source for hobby electronics, check them out today!
+#
+# Written by Rob Hafernik, rob@hafernik.com
+#
+# My first go at this was the "Little Weather Bug", which is documented
+# on GitHub at: https://github.com/robhafernik/LittleWeatherBug
+# 
+# This new version uses the display referenced above, along with the
+# pygame library for python to draw the display.  Some complicaton is 
+# added by having two color levels for everything: one for daytime and 
+# one for nighttime (the day mode is just too bright at night).
+#
+# Released into the public domain, do with it as you will.  It is not,
+# however, warranted to be correct in any way.
+#
+
 os.putenv('SDL_FBDEV', '/dev/fb1')
 
 class BigBug:
